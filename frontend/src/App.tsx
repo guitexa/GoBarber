8e4e589b39505683e3da0212473ae80b0b5ab8d8
Guilherme Teixeira <guilhermetexa@outlook.com>
2020-06-08 01:07:02 -0300

    Authentication on backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { AuthProvider } from './hooks/AuthContext';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn/';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <SignIn />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
   </>
 );
 
