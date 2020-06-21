47ab6ee710cfd638017c6390825902ab07eddeda
Guilherme Teixeira <guilhermetexa@outlook.com>
2020-06-21 12:19:58 -0300

    Auth screen done

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,37 +1,93 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useEffect, useCallback, useState } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgorPassword,
+  ForgorPasswordText,
+  CreateAccount,
+  CreateAccountText,
+} from './styles';
 
 const SignIn: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', keyboardDidShow);
+    Keyboard.addListener('keyboardDidHide', keyboardDidHide);
+
+    // cleanup function
+    return () => {
+      Keyboard.removeListener('keyboardDidShow', keyboardDidShow);
+      Keyboard.removeListener('keyboardDidHide', keyboardDidHide);
+    };
+  }, []);
+
+  const keyboardDidShow = useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const keyboardDidHide = useCallback(() => {
+    setOpen(false);
+  }, []);
+
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Login</Title>
-      <Input
-        name="email"
-        icon="mail"
-        placeholder="E-mail"
-        autoCapitalize="none"
-      />
-      <Input
-        name="password"
-        icon="lock"
-        placeholder="Senha"
-        autoCapitalize="none"
-      />
-      <Button
-        onPress={() => {
-          console.log('ok');
-        }}
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        Entrar
-      </Button>
-    </Container>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Login</Title>
+            </View>
+            <Input
+              name="email"
+              icon="mail"
+              placeholder="E-mail"
+              autoCapitalize="none"
+              onSubmitEditing={Keyboard.dismiss}
+            />
+            <Input
+              name="password"
+              icon="lock"
+              placeholder="Senha"
+              autoCapitalize="none"
+              onSubmitEditing={Keyboard.dismiss}
+            />
+            <Button onPress={() => {}}>Entrar</Button>
+            <ForgorPassword onPress={() => {}}>
+              <ForgorPasswordText>Esqueci minha senha</ForgorPasswordText>
+            </ForgorPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      {!open && (
+        <CreateAccount>
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountText>Criar uma conta</CreateAccountText>
+        </CreateAccount>
+      )}
+    </>
   );
 };
 
--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -11,5 +12,36 @@ export const Title = styled.Text`
   font-size: 24px;
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
-  margin: 50px 0 15px 0;
+  margin: 40px 0 15px 0;
+`;
+
+export const ForgorPassword = styled.TouchableOpacity`
+  margin-top: 10px;
+`;
+
+export const ForgorPasswordText = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  color: #f4ede8;
+`;
+
+export const CreateAccount = styled.TouchableOpacity`
+  position: absolute;
+  width: 100%;
+  height: 50px;
+  background-color: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 10px 0 ${10 + getBottomSpace()}px;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const CreateAccountText = styled.Text`
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 10px;
+  color: #ff9000;
 `;
