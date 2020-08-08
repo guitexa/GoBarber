43387ec47ae3d369c49e969093926c50a4b24c09
Guilherme Teixeira <guilhermetexa@outlook.com>
2020-08-08 10:29:37 -0300

    refactor: change settings

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4356',
+  baseURL: 'http://10.0.2.2:4356',
 });
+
+//http://192.168.0.1:4356
+//http://10.0.2.2:4356
+//http://localhost:4356
 
 export default api;
